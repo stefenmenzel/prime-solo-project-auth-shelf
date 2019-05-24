0a32b72b59f9a972fd4f8b6f3ee32dedca962589
Jesse Gjerde <jessegjerde89@gmail.com>
2019-05-23 23:20:26 -0500

    feature-form complete with reducer started

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -10,17 +10,44 @@ state = {
     img_url: ''
 }
 
+handleSubmit = (event) => {
+event.preventDefault
+this.prop.dispatch({type: "ADD_USER", payload: this.state })
+}
+
+handleDescription = (event) =>{
+    this.setState({ 
+        description: event.target.value
+    })
+
+}
+
+handleImage = (event) => {
+
+    this.setState({
+        img_url: event.target.value
+    })
+}
 
 
     render() {
 
         return (
            
-           <form >
-            <input type= "text" placeholder="description" value=""/>
-            <input type= "text" placeholder="image url" value="" /> 
-            <button type="submit" > Submit </button>
-            </form >
+        <form onSubmit={this.handleSubmit}>
+            <input 
+            onChange={this.handleDescription} 
+            value={this.state.description}
+            type= "text" 
+            placeholder="description" 
+            />
+            <input 
+            type= "text" 
+            placeholder="image url" 
+            value={this.state.img_url}
+            onChange={this.handleImage} /> 
+            <input type="submit" value="submit" />
+        </form>
         )
     }   
 }
