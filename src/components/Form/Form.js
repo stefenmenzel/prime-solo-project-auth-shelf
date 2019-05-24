5b54ad521d4ca69aa4300300673832c748c0198a
Jesse Gjerde <jessegjerde89@gmail.com>
2019-05-24 09:12:09 -0500

    comments made

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,25 +5,27 @@ import { connect } from 'react-redux'
 
 class Form extends ReactComponent {
 
+    // declaring state
 state = {
     description: '',
     img_url: ''
 }
 
+ // handle submit
 handleSubmit = (event) => {
 event.preventDefault
-this.prop.dispatch({type: "ADD_USER", payload: this.state })
+this.prop.dispatch({type: "ADD_IMAGE", payload: this.state })
 }
 
+// changes state for description
 handleDescription = (event) =>{
     this.setState({ 
         description: event.target.value
     })
 
 }
-
+// changes state for img_url
 handleImage = (event) => {
-
     this.setState({
         img_url: event.target.value
     })
@@ -33,7 +35,7 @@ handleImage = (event) => {
     render() {
 
         return (
-           
+            // form for image_url and description
         <form onSubmit={this.handleSubmit}>
             <input 
             onChange={this.handleDescription} 
@@ -46,11 +48,12 @@ handleImage = (event) => {
             placeholder="image url" 
             value={this.state.img_url}
             onChange={this.handleImage} /> 
-            <input type="submit" value="submit" />
+            <button type="submit" value="submit">Submit</button> 
         </form>
         )
     }   
 }
+
 
 const mapReduxState = (reduxState) => {
     return {
